e4a8ffba21454f80bba6a73f3b1908cf45ecea17
Rufus <rufusasterisk@gmail.com>
2017-08-22 19:42:47 -0600

    Add foreach tests for all obstacles

--- test/obstacle-test.js
+++ test/obstacle-test.js
@@ -2,23 +2,37 @@ var assert = require('chai').assert;
 var {Obstacle, Auto, Tractor, SlowCar, Semi, FastCar, Snake} = require('../lib/obstacle.js');
 
 
-describe('Obstacle', function() {
-  var myObstacle;
+describe('Obstacle & subclasses', function() {
+  // var myObstacle;
+  var myObstacleArray = [];
 
   beforeEach(function() {
-    myObstacle = new Obstacle(670, 500, 67, -10, 67, 67)
+    // myObstacle = new Obstacle(670, 500, 67, -10, 67, 67)
+    myObstacleArray = [
+      new Obstacle(670, 500, 67, -10, 67, 67),
+      new Auto(670, 500, 67, -10, 67, 67),
+      new Tractor(670, 500, 67, -10, 67, 67),
+      new SlowCar(670, 500, 67, -10, 67, 67),
+      new Semi(670, 500, 67, -10, 67, 67),
+      new FastCar(670, 500, 67, -10, 67, 67),
+      new Snake(670, 500, 67, -10, 67, 67)]
   })
 
   it('should create an object', function() {
-    assert.isObject(myObstacle);
+    myObstacleArray.forEach(function(myObstacle) {
+      assert.isObject(myObstacle);
+    })
   })
 
   it('should move', function() {
-    assert.equal(myObstacle.xCoordinate, 670);
-    assert.equal(myObstacle.yCoordinate, 500);
-    let targetx = myObstacle.xCoordinate + myObstacle.velocity;
-    myObstacle.moveObstacle();
-    assert.equal(myObstacle.xCoordinate, targetx);
-    assert.equal(myObstacle.yCoordinate, 500);
+    myObstacleArray.forEach(function(myObstacle) {
+      let targetx = myObstacle.xCoordinate + myObstacle.velocity;
+
+      assert.equal(myObstacle.xCoordinate, 670);
+      assert.equal(myObstacle.yCoordinate, 500);
+      myObstacle.moveObstacle();
+      assert.equal(myObstacle.xCoordinate, targetx);
+      assert.equal(myObstacle.yCoordinate, 500);
+    })
   })
 })
