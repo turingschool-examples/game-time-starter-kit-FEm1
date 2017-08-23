9ab42eb6e8638bce5127e8c33f868b37558e5fd1
Rufus <rufusasterisk@gmail.com>
2017-08-23 13:55:59 -0600

    Add timer tick and death tests and functions

--- test/level-test.js
+++ test/level-test.js
@@ -1,6 +1,5 @@
 var assert = require('chai').assert;
 var Level = require('../lib/level.js');
-var Toad = require('../lib/toad.js');
 
 describe('Level', function() {
   var myLevel;
@@ -23,5 +22,29 @@ describe('Level', function() {
 
   it('Should start at Level 1', function() {
     assert.equal(myLevel.currentLevel, 1);
+  })
+
+  it('Should decrease lives when Toad dies', function() {
+    assert.equal(myLevel.lives, 5);
+    myLevel.death();
+    assert.equal(myLevel.lives, 4);
+  })
+
+  it('Should be "Game Over" if death at 0 lives', function() {
+    assert.equal(myLevel.lives, 5);
+    for (let i = 0; i < 5; i++) {
+      myLevel.death();
+    }
+    assert.equal(myLevel.lives, 0);
+    myLevel.death();
+    assert.equal(myLevel.gameOver, true);
+  })
+
+  it('Should decrease timer by .25 seconds every 15 frames', function() {
+    assert.equal(myLevel.timeRemaining, 60);
+    for (let i = 0; i < 15; i++) {
+      myLevel.drawTimer();
+    }
+    assert.equal(myLevel.timeRemaining, 59.75);
   })
 })
