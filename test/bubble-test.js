96bd5259883fe2a60df7924e1f466f8467817912
Jordan Quinn <jpquinn605@gmail.com>
2017-12-05 15:45:38 -0700

    Add testing files

---
+++ test/bubble-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const Bubble = require('../lib/Bubble.js');
+const assert = chai.assert;
+
+
+describe('Bubble', function() {
+  it('should return true', function() {
+    assert.equal('true', 'true')
+  });
+
+  
+})
